e0cfe9b13bfc22ca1503659b36cd9acfa6d430e2
wyrla <111397148+wyrla@users.noreply.github.com>
2024-03-31 20:11:52 -0300

    refactor: slipt zsh config into multiple files

---
+++ zsh/alias.zsh
@@ -0,0 +1,4 @@
+alias w="cd ~/enterprise"
+alias p="cd ~/projects"
+alias repair="sudo chown -R "${USER:-$(id -un)}" . "
+alias dotf="~/dotfiles"
---
+++ zsh/plugins.zsh
@@ -0,0 +1,8 @@
+plugins=(
+    dotenv
+    git
+    ssh-agent
+    zsh-autosuggestions
+    zsh-completions
+    zsh-syntax-highlighting
+)
