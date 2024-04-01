e19e10199b49d9145978f57bd23d63fa2f1d003e
wyrla <111397148+wyrla@users.noreply.github.com>
2024-03-31 23:43:24 -0300

    feat: create vscode extensions script

---
+++ vscode/extensions.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+extensions=(
+    catppuccin.catppuccin-vsc
+    formulahendry.code-runner
+    gruntfuggly.todo-tree
+    mechatroner.rainbow-csv
+    eamodio.gitlens
+    mhutchie.git-graph
+    mongodb.mongodb-vscode
+    smcpeak.default-keys-windows
+    streetsidesoftware.code-spell-checker
+    streetsidesoftware.code-spell-checker-portuguese-brazilian
+    wakatime.vscode-wakatime
+)
+
+if type -p code > /dev/null; then
+    for extension in "${extensions[@]}"; do
+        code --install-extension $extension
+    done
+else
+    echo "VSCode installation missing"
+fi
