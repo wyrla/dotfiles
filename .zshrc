eacd9b1373cad22d3fa39e811a603883629567e2
wyrla <111397148+wyrla@users.noreply.github.com>
2024-04-01 10:33:17 -0300

    fix: typos fixes

--- .zshrc
+++ .zshrc
@@ -1,3 +1,3 @@
 source "$HOME/dotfiles/zsh/alias.zsh"
-source "$HOME/dotfiles/zsh/general.zsh"
 source "$HOME/dotfiles/zsh/plugins.zsh"
+source "$HOME/dotfiles/zsh/general.zsh"
--- setup/symlinks.sh
+++ setup/symlinks.sh
@@ -5,4 +5,4 @@ create_symlink ~/dotfiles/.zshrc  ~/.zshrc
 create_symlink ~/dotfiles/.gitconfig ~/.gitconfig
 create_symlink ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json 
 create_symlink ~/dotfiles/dunst-config ~/.config/dunst/dunstrc
-create_symlink ~/dotfiles/rofi-config.rasi  ~/.config/rofi/config.rasi
+create_symlink ~/dotfiles/rofi/config.rasi  ~/.config/rofi/config.rasi
