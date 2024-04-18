eda987107885dcca35558d67cbf307a8ec1ae46d
wyrla <111397148+wyrla@users.noreply.github.com>
2024-04-18 08:56:53 -0300

    feat: create symbolic link to catppuccin rofi theme

--- setup/symlinks.sh
+++ setup/symlinks.sh
@@ -5,4 +5,5 @@ create_symlink ~/dotfiles/.zshrc  ~/.zshrc
 create_symlink ~/dotfiles/.gitconfig ~/.gitconfig
 create_symlink ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json 
 create_symlink ~/dotfiles/dunst-config ~/.config/dunst/dunstrc
+create_symlink ~/dotfiles/rofi/catppuccin-latte.rasi  ~/.config/rofi/catppuccin-latte.rasi
 create_symlink ~/dotfiles/rofi/config.rasi  ~/.config/rofi/config.rasi
