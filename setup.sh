3b176b75837f13d88dff3906417916705eee2d10
wyrla <111397148+wyrla@users.noreply.github.com>
2024-09-29 19:48:29 -0300

    refactor: source vscode extensions intallation

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@ source $HOME/dotfiles/setup/applications.sh
 source $HOME/dotfiles/setup/symlinks.sh
 source $HOME/.zshrc
 source $HOME/dotfiles/setup/general.sh
+
+source $HOME/dotfiles/vscode/extensions.sh
